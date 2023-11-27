77e0524e640d140d2335ce9508c2369facef855a
st184914 <st184914@stud.uni-stuttgart.de>
2023-11-27 20:15:38 +0100

    preprocessing of input to gradcam done, output images in gray scale

--- diabetic_retinopathy/deep_visu/deep_visualise.py
+++ diabetic_retinopathy/deep_visu/deep_visualise.py
@@ -7,7 +7,10 @@ import tensorflow as tf
 import cv2
 import logging
 
+from datetime import datetime
 from deep_visu.grad_cam import GradCam
+from input_pipeline.tfrecords import preprocess_image, convert_to_binary
+from input_pipeline.preprocessing import preprocess
 
 @gin.configurable
 class DeepVisualize:
@@ -43,8 +46,8 @@ class DeepVisualize:
         images_train = [self.load_and_preprocess(file_path) for file_path in file_paths_train]
         images_test = [self.load_and_preprocess(file_path) for file_path in file_paths_test]
 
-        self.convert_to_binary(df_train)
-        self.convert_to_binary(df_test)
+        convert_to_binary(df_train)
+        convert_to_binary(df_test)
         labels_train = df_train['Retinopathy grade'].values
         labels_test = df_test['Retinopathy grade'].values
 
@@ -54,46 +57,38 @@ class DeepVisualize:
 
     def load_and_preprocess(self, img_path):
         image = cv2.imread(img_path)
-        image = self.preprocess_image(image)
+        image = preprocess_image(image)
+        image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
         return image
 
-    def preprocess_image(self, image):
-        gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-        _, threshold = cv2.threshold(gray, 15, 255, cv2.THRESH_BINARY)
-        pos = np.nonzero(threshold)
-        right_boundary = pos[1].max()
-        left_boundary = pos[1].min()
-        image = image[:, left_boundary:right_boundary]
-        # computations to obtain square image (padding at desired positions)
-        upper_diff = (image.shape[1] - image.shape[0]) // 2
-        lower_diff = image.shape[1] - image.shape[0] - upper_diff
-        image = cv2.copyMakeBorder(image, upper_diff, lower_diff, 0, 0, cv2.BORDER_CONSTANT)
-        return cv2.resize(image, (256, 256))
-
-    def convert_to_binary(self, df):
-        df.loc[df['Retinopathy grade'] < 2, "Retinopathy grade"] = 0
-        df.loc[df['Retinopathy grade'] > 1, "Retinopathy grade"] = 1
-        return df
-
     def visualize(self):
+        logging.info("===============Starting Deep Visualisation================")
         ds_train, ds_test = self.create_dataset()
+        ds_train = ds_train.map(preprocess, num_parallel_calls=tf.data.experimental.AUTOTUNE)
+        ds_test = ds_test.map(preprocess, num_parallel_calls=tf.data.experimental.AUTOTUNE)
+        logging.info("dataset created from image checkpoints")
         checkpoint = tf.train.Checkpoint(model=self.model)
         checkpoint.restore(tf.train.latest_checkpoint(self.run_paths['path_ckpts_train']))
+        logging.info(f"model loaded with checkpoint from {self.run_paths['path_ckpts_train']}")
+        # checkpoint.restore(tf.train.latest_checkpoint('/home/RUS_CIP/st184914/dl-lab-23w-team10/experiments/run_2023-11-27T19-00-24-470804/ckpts'))
 
         output_dir = os.path.join(self.target_dir, 'gradcam_out')
-        # Create "gradcam_out" directory if it doesn't exist
         os.makedirs(output_dir, exist_ok=True)
 
-        gradcam_out_dir = self.target_dir + "/gradcam_out/"
+        timestamp = datetime.now().strftime("%Y-%m-%d_%H:%M:%S")
+        out_dir = f"out_at_{timestamp}"
+        gradcam_out_dir = self.target_dir + "gradcam_out/" + f"{out_dir}/"
+        os.makedirs(gradcam_out_dir)
+
         train_dir = os.path.join(gradcam_out_dir, 'train')
         os.makedirs(train_dir, exist_ok=True)
 
         test_dir = os.path.join(gradcam_out_dir, 'test')
         os.makedirs(test_dir, exist_ok=True)
 
-        logging.info("===============applying gradCAM================")
+        logging.info("applying gradCAM")
         gradcam_test = GradCam(self.model, self.layer_name, ds_test)
         gradcam_train = GradCam(self.model, self.layer_name, ds_train)
         gradcam_train.apply_gradcam(self.image_list_train, train_dir)
         gradcam_test.apply_gradcam(self.image_list_test, test_dir)
-        logging.info("=================gradCAM ended=================")
+        logging.info(f"images saved in {gradcam_out_dir}")
--- diabetic_retinopathy/deep_visu/grad_cam.py
+++ diabetic_retinopathy/deep_visu/grad_cam.py
@@ -3,6 +3,7 @@ import numpy as np
 import tensorflow as tf
 import matplotlib.cm as cm
 import os
+import cv2
 
 
 @gin.configurable
@@ -14,7 +15,8 @@ class GradCam:
         self.class_idx = class_idx
         self.grad_model = tf.keras.models.Model(self.model.inputs,
                                                 [self.model.get_layer(name=self.layer_name).output, self.model.output])
-
+    
+    @tf.function
     def get_average_grads(self, image):
         with tf.GradientTape() as tape:
             layer_activations, preds = self.grad_model(image)
@@ -26,6 +28,7 @@ class GradCam:
         average_grads = tf.reduce_mean(grads, axis=(0, 1, 2))
         return average_grads, layer_activations
 
+
     def get_heatmap(self, image):
         average_grads, layer_activations = self.get_average_grads(image)
         average_grads = tf.expand_dims(average_grads, axis=-1)
@@ -33,6 +36,7 @@ class GradCam:
         heatmap = tf.squeeze(heatmap)
         heatmap = tf.maximum(heatmap, 0) / tf.reduce_max(heatmap)
         return heatmap.numpy()
+
 
     def get_jet_heatmap(self, heatmap, image):
         heatmap = np.uint8(255 * heatmap)
@@ -42,19 +46,19 @@ class GradCam:
         jet_heatmap = tf.keras.utils.array_to_img(jet_heatmap)
         jet_heatmap = jet_heatmap.resize((image.shape[1], image.shape[0]))
         jet_heatmap = tf.keras.utils.img_to_array(jet_heatmap)
+        jet_heatmap = jet_heatmap/255.0
         return jet_heatmap
 
-    def apply_gradcam(self, image_list, save_dir, alpha=0.5):
-        dataset = self.dataset
 
+    def apply_gradcam(self, image_list, save_dir, alpha=0.2):
+        dataset = self.dataset
         for i, (image, label) in enumerate(dataset):
             img_idx = image_list[i]
             image = tf.expand_dims(image, axis=0)
             heatmap = self.get_heatmap(image)
-            image = tf.squeeze(image)
+            image = tf.squeeze(image, axis=0)
             jet_heatmap = self.get_jet_heatmap(heatmap, image)
+            image = tf.image.rgb_to_grayscale(image)
             superimposed_img = jet_heatmap * alpha + image
-            # superimposed_img = tf.keras.utils.array_to_img(superimposed_img)
-            # superimposed_img = tf.keras.utils.img_to_array(superimposed_img)
             save_path = os.path.join(save_dir, f"img_{img_idx}_label-{label}.jpg")
             tf.keras.preprocessing.image.save_img(save_path, superimposed_img)
