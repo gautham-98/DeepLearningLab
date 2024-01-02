605b284509cf104918a906be601052428d60fa24
st184584 <st184584@stud.uni-stuttgart.de>
2024-01-02 14:00:02 +0100

    add input dim from main.py

--- human_activity_recognition/models/architectures.py
+++ human_activity_recognition/models/architectures.py
@@ -9,17 +9,23 @@ from tensorflow.keras import layers, Sequential
 
 
 @gin.configurable
-def model1_LSTM(input_shape):
+def model1_LSTM(window_length, dropout=0.3):
     
-    model = keras.Sequential([keras.Input(shape=input_shape)])
+    model = keras.Sequential([keras.Input(shape=(window_length,6))])
     model.add(layers.LSTM(128, return_sequences=True))
-    model.add(layers.Dropout(0.2))
+    model.add(layers.Dropout(dropout))
+    model.add(layers.BatchNormalization())
+    model.add(layers.LSTM(128, return_sequences=True))
+    model.add(layers.Dropout(dropout))
+    model.add(layers.BatchNormalization())
+    model.add(layers.LSTM(128, return_sequences=True))
+    model.add(layers.Dropout(dropout))
     model.add(layers.BatchNormalization())
     model.add(layers.LSTM(128))
-    model.add(layers.Dropout(0.2))
+    model.add(layers.Dropout(dropout))
     model.add(layers.BatchNormalization())
     model.add(layers.Dense(64 , activation="relu"))
-    model.add(layers.Dropout(0.2))
+    model.add(layers.Dropout(dropout))
     model.add(layers.Dense(12, activation="linear"))
     model.build()
 
