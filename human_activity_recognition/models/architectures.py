dc6754409117ee3fd24ce52ddbc4945173d81279
st184584 <st184584@stud.uni-stuttgart.de>
2024-01-05 00:09:45 +0100

    change output shape

--- human_activity_recognition/models/architectures.py
+++ human_activity_recognition/models/architectures.py
@@ -10,7 +10,7 @@ def one_lstm_layer(lstm_cells, dropout_rate):
     return layers.LSTM(lstm_cells, dropout=dropout_rate,return_sequences=True)
 
 @gin.configurable
-def model1_LSTM(window_length,num_lstm,dense_units,lstm_cells ,dropout_rate=0.3):
+def model1_LSTM(window_length,num_lstm,dense_units,lstm_cells,n_classes,dropout_rate=0.3):
     model = keras.Sequential([keras.Input(shape=(window_length,6))])
     for e in range(num_lstm):
         layer = one_lstm_layer(lstm_cells, dropout_rate=dropout_rate)
@@ -20,8 +20,10 @@ def model1_LSTM(window_length,num_lstm,dense_units,lstm_cells ,dropout_rate=0.3)
     model.add(layers.BatchNormalization())
     model.add(layers.Dense(dense_units , activation="relu"))
     model.add(layers.Dropout(dropout_rate))
-    model.add(layers.Dense(12, activation="linear"))
+    model.add(layers.Dense(n_classes, activation="linear"))
     model.build()
+    print(model.input_shape)
+    print(model.output_shape)
 
 
     return model
